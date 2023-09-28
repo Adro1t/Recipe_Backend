b5c7a76db9dd3be6dd2288fa8d9c1c143218bee3
XxPlayerxX <ramankhadgi0@gmail.com>
2023-09-28 13:49:22 +0545

    categoryList and by id

--- controller/category.js
+++ controller/category.js
@@ -4,16 +4,9 @@ exports.helloController = (req, res) => {
   res.send("hello from category controller");
 };
 
+//to insert
 exports.postCategory = (req, res) => {
   const category = new Category(req.body);
-
-  // category.save((error, category) => {
-  //   if (error || !category) {
-  //     res.status(400).json({ error: "failed to insert category" });
-  //   }
-  //   res.json({ category });
-  // });
-
   category
     .save()
     .then((savedCategory) => {
@@ -22,4 +15,32 @@ exports.postCategory = (req, res) => {
     .catch((error) => {
       res.status(400).json({ error: "failed to insert category" });
     });
+};
+
+//to fetch
+exports.getCategoryList = (req, res) => {
+  Category.find()
+    .then((result) => {
+      res.json({ result });
+    })
+    .catch((error) => {
+      res.status(400).json({ error: "failed to fetch" });
+    });
+};
+
+//single category by id
+exports.getSingleCategory = (req, res, next, id) => {
+  Category.findById(id)
+    .then((category) => {
+      req.category = category;
+      next();
+    })
+    .catch((error) => {
+      res.status(400).json({ error: "failed to find category" });
+    });
+};
+
+//to fetch categpry data related to single id
+exports.getCategoryDetail = (req, res) => {
+  res.json(req.category);
 };
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const { helloController, postCategory } = require("../controller/category");
+const {
+  helloController,
+  postCategory,
+  getCategoryList,
+  getSingleCategory,
+  getCategoryDetail,
+} = require("../controller/category");
 const router = express.Router();
 
 router.get("/", helloController);
 router.post("/postcategory", postCategory);
+router.get("/categorylist", getCategoryList);
+router.get("/categorydetail/:categoryId", getCategoryDetail);
+
+//to get single category value in param
+router.param("categoryId", getSingleCategory);
 
 module.exports = router;
